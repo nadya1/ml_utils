ba08236795cb39c80706d605442f38dd5e033dc9
Nadya K <nadyaortiz@gmail.com>
2017-05-21 17:07:32 -0700

    add helper function: find-ecludian distance, compute rss, etc...

--- src/ml_numpy_utils.py
+++ src/ml_numpy_utils.py
@@ -3,67 +3,92 @@ __date__ = '04/09/2017'
 
 import numpy as np
 
-def get_numpy_data(data_sframe, features, output):
-    """Convert SFrame to Numpy Array"""
+def compute_RSS(predictions,output):
+	"""Residual Sum of Squares (RSS)"""
+	residuals = predictions - output
+	RSS = (residuals ** 2).sum()
+	return (RSS)
 
-    #Add constant
-    data_sframe['constant'] = 1  #constant column (to  create an 'intercept')
-    features = ['constant'] + features  # add the column 'constant' to the front of the features list
+def get_numpy_data(data_sframe, features, output, constant='constant'):
+	"""Convert SFrame to Numpy Array"""
 
-    # select the columns of data_SFrame given by the features list sframe
-    features_sframe = data_sframe[features]
+	#Add constant
+	data_sframe[constant] = 1  #constant column (to  create an 'intercept')
+	features = [constant] + features  # add the column 'constant' to the front of the features list
 
-    #Convert to numpy-matrix
-    feature_matrix = features_sframe.to_numpy() # convert features_SFrame into a numpy matrix:
-    output_sarray = data_sframe[output] ## assign the column of data_sframe associated with the output
-    output_array = output_sarray.to_numpy() # convert the SArray into a numpy array
+	# select the columns of data_SFrame given by the features list sframe
+	features_sframe = data_sframe[features]
 
-    return (feature_matrix, output_array)
+	#Convert to numpy-matrix
+	feature_matrix = features_sframe.to_numpy() # convert features_SFrame into a numpy matrix:
+	output_sarray = data_sframe[output] ## assign the column of data_sframe associated with the output
+	output_array = output_sarray.to_numpy() # convert the SArray into a numpy array
+
+	return (feature_matrix, output_array)
 
 def predict_output(feature_matrix, weights):
-    """Predicting output given regression weights"""
-    predictions = np.dot(feature_matrix, weights)
-    return (predictions)
+	"""Predicting output given regression weights"""
+	predictions = np.dot(feature_matrix, weights)
+	return (predictions)
 
 def feature_derivative(errors, feature):
-    """Computing derivative: dot product of these vectors(np.arrays) """
-    derivative =  2 * np.dot(errors,feature)
-    return(derivative)
+	"""Computing derivative: dot product of these vectors(np.arrays) """
+	derivative =  2 * np.dot(errors,feature)
+	return(derivative)
 
 def feature_derivative_ridge(errors, feature, weight, l2_penalty, feature_is_constant):
-    """  If feature_is_constant is True, derivative is 
-         twice the dot product of errors and feature"""
-    derivative = 2 * np.dot(errors,feature)
-    if not feature_is_constant:
-        derivative += 2*l2_penalty*weight
-    return derivative
+	"""  If feature_is_constant is True, derivative is 
+		 twice the dot product of errors and feature"""
+	derivative = 2 * np.dot(errors,feature)
+	if not feature_is_constant:
+		derivative += 2*l2_penalty*weight
+	return derivative
 
 def compute_cost_function(errors, weights, l2_penalty):
-    """sum data points of the sqd-doff btw observed-vs-predicted + L2 penalty
-    Cost(w) = SUM[(prediction - output) ^ 2]  + l2_penalty * (w[0] ^ 2 + ... + w[k] ^ 2).
-    applying derivate: 
-    Cost(w) = 2*SUM[ error*[feature_i] ] + 2*l2_penalty*w[i].            
-    """
-    return np.dot(errors,errors) + l2_penalty * (np.dot(weights,weights) - weights[0] ** 2)
+	"""sum data points of the sqd-doff btw observed-vs-predicted + L2 penalty
+	Cost(w) = SUM[(prediction - output) ^ 2]  + l2_penalty * (w[0] ^ 2 + ... + w[k] ^ 2).
+	applying derivate: 
+	Cost(w) = 2*SUM[ error*[feature_i] ] + 2*l2_penalty*w[i].            
+	"""
+	return np.dot(errors,errors) + l2_penalty * (np.dot(weights,weights) - weights[0] ** 2)
 
 def print_coefficients(model):
-    """print format: x^n.....x^2"""
-    w = list(model.coefficients['value'])
-    w.reverse()
-    print "\nLearned polynomial:"
-    print np.poly1d(w)
-    return w
+	"""print format: x^n.....x^2"""
+	w = list(model.coefficients['value'])
+	w.reverse()
+	print "\nLearned polynomial:"
+	print np.poly1d(w)
+	return w
 
 def normalize_features(feature_matrix, axis=0):
-    norms = np.linalg.norm(feature_matrix, axis=axis)
-    normalized_features = feature_matrix / norms
-    return (normalized_features, norms)
+	norms = np.linalg.norm(feature_matrix, axis=axis)
+	normalized_features = feature_matrix / norms
+	return (normalized_features, norms)
 
 def get_normalized_data(dataset,features,target):
-    feature_matrix,output = get_numpy_data(dataset,features,target)
-    feature_matrix_norm,norms = normalize_features(feature_matrix)
-    return feature_matrix_norm, output, norms
+	feature_matrix,output = get_numpy_data(dataset,features,target)
+	feature_matrix_norm,norms = normalize_features(feature_matrix)
+	return feature_matrix_norm, output, norms
 
 def get_nonzero_weights(weights):
-    weights_nnz = np.array(weights).nonzero()
-    return weights[weights_nnz]
+	weights_nnz = np.array(weights).nonzero()
+	return weights[weights_nnz]
+
+def get_euclidean_distance(source, query):
+	"""Euclidean norm is the L2 norm or L2 distance."""
+	return np.sqrt(np.sum((source - query) ** 2))
+
+def get_euclidean_distance_matrix(source, matrix_query, axis=1):
+	"""Euclidean norm is the L2 norm  (np.matrix-vectors)
+	   return a list-distances. (np.argmin to find min-idx-distance )"""
+	return np.sqrt(np.sum((source - matrix_query) ** 2, axis=axis))
+
+def find_k_nearest_neighbors(k, source, matrix_query, axis=1):
+	"""returns the indices of the k closest training sorce."""
+	return np.argsort(get_euclidean_distance_matrix(source, matrix_query, axis))[:k]
+
+def single_prediction_k_nearest_neighbors(k, source, matrix_query, output_values):
+	""" extract multiple values using a list of indices 
+		e.g: output_train[[6, 10]] """
+	k_nearest = find_k_nearest_neighbors(k, source, matrix_query)
+	return np.average(output_values[k_nearest])
